1abc6b75d03e1ddb285fd90a46fe20484d333249
Eric <elandau@protonmail.com>
2020-08-10 13:33:38 +0100

    Changing tuple

--- detectron2/layers/wrappers.py
+++ detectron2/layers/wrappers.py
@@ -223,4 +223,4 @@ def nonzero_tuple(x):
     """
     if x.dim() == 0:
         return torch.nonzero(x, as_tuple=False).unsqueeze(0).unbind(1)
-    return torch.nonzero(x, as_tuple=True).unbind(1)
+    return torch.nonzero(x, as_tuple=False).unbind(1)
